1f15da299db6fea367b23df64e2d3e839c7f0b7b
Dan Skiba <skdenx@gmail.com>
2023-12-03 22:52:52 +0100

    add 1 part 2

--- src/1/index.test.ts
+++ src/1/index.test.ts
@@ -2,8 +2,15 @@ import { expect, test } from 'bun:test'
 import { solve } from './index'
 
 const input = Bun.file('./test-input.txt')
-test("should solve 1", async () => {
+test("should solve part 1", async () => {
   const text = await input.text()
-  const res = solve(text.trim())
+  const res = solve(text.trim(), false)
   expect(res).toBe(142);
+})
+
+const input2 = Bun.file('./test-input-2.txt')
+test("should solve part 2", async () => {
+  const text = await input2.text()
+  const res = solve(text.trim(), true)
+  expect(res).toBe(281);
 })
--- src/1/index.ts
+++ src/1/index.ts
@@ -1,32 +1,67 @@
-import { expect, test } from "bun:test";
-
-function isNumber(value: string): boolean {
-  return !isNaN(Number(value));
+function isNumber(value: string | number | boolean): boolean {
+  return !isNaN(Number(value))
 }
 
-export function solve(input: string): number {
-  let splitted = input.split("\n");
-  const lines = splitted.map((line) => {
-    let pair: [number, number] = [0,0];
-    for (let i = 0; i < line.length; i++) {
-      const char = line[i];
-      if(isNumber(char)) {
-        let number = Number(char);
-        if(pair[0]) {
+export function solve(input: string, withSubstr: boolean): number {
+  let splitted = input.split('\n')
+  const lines = splitted.map((lineStr): number => {
+    let pair: [number, number] = [0, 0]
+
+    for (let i = 0; i < lineStr.length; i++) {
+      const char = lineStr[i]
+      if (isNumber(char)) {
+        let number = Number(char)
+        if (pair[0]) {
           pair[1] = number
         } else {
           pair[0] = number
           pair[1] = number
         }
+        continue
+      }
+      if (withSubstr) {
+        const subStr = includesStrNum(lineStr, i)
+        if (subStr) {
+          if (pair[0]) {
+            pair[1] = subStr
+          } else {
+            pair[0] = subStr
+            pair[1] = subStr
+          }
+        }
       }
     }
-    return Number(`${pair[0]}${pair[1]}`);
-  });
 
-return lines.reduce((a, b) => a + b, 0);
+    return Number(`${pair[0]}${pair[1]}`)
+  })
+  return lines.reduce((a, b) => a + b, 0)
+}
+
+const NUM_MAP = {
+  'one': 1,
+  'two': 2,
+  'three': 3,
+  'four': 4,
+  'five': 5,
+  'six': 6,
+  'seven': 7,
+  'eight': 8,
+  'nine': 9
+} as const
+
+const keys = Object.keys(NUM_MAP) as (keyof typeof NUM_MAP)[]
+
+function includesStrNum(str: string, idx: number): number {
+  for (const key of keys) {
+    if (key === str.substring(idx, idx + key.length)) {
+      return NUM_MAP[key]
+    }
+  }
+  return 0
 }
 
 
-const text = await Bun.file("./input.txt").text();
+const text = await Bun.file('./input.txt').text()
 
-await Bun.write("./result.txt", String(solve(text.trim())));
+await Bun.write('./result.txt', String(solve(text.trim(), false)))
+await Bun.write('./result-2.txt', String(solve(text.trim(), true)))
